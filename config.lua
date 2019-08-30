684ea8f348fe17a7d643dd2aeb627b588dae7685
Neddings <github@neddings.com>
2019-08-30 15:48:49 +0200

    Updating locations

--- config.lua
+++ config.lua
@@ -2,8 +2,52 @@ Config              = {}
 Config.Locale       = 'en'
 
 -- Cloak room
-Config.Cloakroom
+Config.Cloakroom    =   {
+    Pos =   {x = , y =, z =}
+}
 -- Location of fruitstands
-Config.Stands
+Config.Stands   =   {
+    Stand1 = {
+        Pos   = {x = -3026.036, y = 369.6749, z = 13.5693},
+    },
+    Stand2 = {
+        Pos   = {x = -1553.516, y = 1373.586, z = 125.76},
+    },
+    Stand3 = {
+        Pos   = {x = 1045.173, y = 696.9411, z = 157.8391},
+    },
+    Stand4 = {
+        Pos   = {x = -1380.767, y = 734.5928, z = 181.9658},
+    },
+    Stand5 = {
+        Pos   = {x = 2527.412, y = 2038.055, z = 18.80867},
+    },
+    Stand6 = {
+        Pos   = {x = 1088.167, y = 6510.437, z = 20.07833},
+    },
+    Stand7 = {
+        Pos   = {x = -1043.469, y = 5326.102, z = 43.6015},
+    },
+    Stand8 = {
+        Pos   = {x = 2474.29, y = 4445.75, z = 34.38753},
+    },
+    Stand9 = {
+        Pos   = {x = -2510.075, y = 3615.438, z = 12.65181},
+    },
+    Stand10 = {
+        Pos   = {x = -458.8947, y = 2863.294, z = 34.10668},
+    },
+    Stand11 = {
+        Pos   = {x = 1263.157, y = 3547.175, z = 34.14659},
+    },
+    Stand12 = {
+        Pos   = {x = 1476.846, y = 2723.044, z = 36.64265},
+    },
+    Stand13 = {
+        Pos   = {x = 149.0806, y = 1669.901, z = 227.6887},
+    },
+}
 -- Location of pickup fruits
-Config.Pickup
+Config.Pickup =     {
+
+}
